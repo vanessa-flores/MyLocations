f96af02394e8f6ad6021803f1dcbefa34ffa5602
Vanessa Flores <floresv26@gmail.com>
2020-09-01 11:59:57 -0500

    Set CurrentLocationViewController UI

--- MyLocations/View Controllers/CurrentLocationViewController.swift
+++ MyLocations/View Controllers/CurrentLocationViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class CurrentLocationViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet private weak var messageLabel: UILabel!
+    @IBOutlet private weak var latitudeLabel: UILabel!
+    @IBOutlet private weak var longitudeLabel: UILabel!
+    @IBOutlet private weak var addressLabel: UILabel!
+    @IBOutlet private weak var tagButton: UIButton!
+    @IBOutlet private weak var getButton: UIButton!
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: - Actions
+    
+    @IBAction private func getLocation() {
+        // do nothing yet
+    }
 }
 
