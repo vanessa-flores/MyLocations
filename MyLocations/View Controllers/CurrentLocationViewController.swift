3f60312a00ba46cc24a4d711abc5483f232b37b5
Vanessa Flores <floresv26@gmail.com>
2020-09-01 14:45:20 -0500

    Get results for a specific accuracy level

--- MyLocations/View Controllers/CurrentLocationViewController.swift
+++ MyLocations/View Controllers/CurrentLocationViewController.swift
@@ -74,9 +74,25 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         let newLocation = locations.last!
         print("didUpdateLocations \(newLocation)")
         
-        location = newLocation
-        lastLocationError = nil
-        updateLabels()
+        if newLocation.timestamp.timeIntervalSinceNow < -5 {
+            return
+        }
+        
+        if newLocation.horizontalAccuracy < 0 {
+            return
+        }
+        
+        if location == nil || location!.horizontalAccuracy > newLocation.horizontalAccuracy {
+            lastLocationError = nil
+            location = newLocation
+            
+            if newLocation.horizontalAccuracy <= locationManager.desiredAccuracy {
+                print("*** We're done!")
+                stopLocationManager()
+            }
+            
+            updateLabels()
+        }
     }
     
     // MARK: - Helpers
