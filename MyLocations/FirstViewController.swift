22863a4fd433777dc42680d6c5a47f1af15ecfc0
Vanessa Flores <floresv26@gmail.com>
2020-09-01 11:07:00 -0500

    Project setup

---
+++ MyLocations/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Vanessa Flores on 9/1/20.
+//  Copyright © 2020 Rising Dev Habits. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
