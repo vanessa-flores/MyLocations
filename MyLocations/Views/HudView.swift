274e4d33d8e36d1662a27b4e50f04007e00535f8
Vanessa Flores <floresv26@gmail.com>
2020-09-02 14:23:41 -0500

    Improve animation

--- MyLocations/Views/HudView.swift
+++ MyLocations/Views/HudView.swift
@@ -61,10 +61,14 @@ class HudView: UIView {
             alpha = 0
             transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
             
-            UIView.animate(withDuration: 0.3, animations: {
+            UIView.animate(withDuration: 0.3, delay: 0,
+                           usingSpringWithDamping: 0.7,
+                           initialSpringVelocity: 0.5,
+                           options: [],
+                           animations: {
                 self.alpha = 1
                 self.transform = CGAffineTransform.identity
-            })
+            }, completion: nil)
         }
     }
 }
