68174c7858cfdb4a6cd8ddb158314c96a3673f26
Vanessa Flores <floresv26@gmail.com>
2020-09-02 14:12:08 -0500

    Display hud checkmark

--- MyLocations/Views/HudView.swift
+++ MyLocations/Views/HudView.swift
@@ -21,6 +21,12 @@ class HudView: UIView {
         
         UIColor(white: 0.3, alpha: 0.8).setFill()
         roundedRect.fill()
+        
+        // Draw checkmark
+        if let image = UIImage(named: "Checkmark") {
+            let imagePoint = CGPoint(x: center.x - round(image.size.width / 2), y: center.y - round(image.size.height / 2) - boxHeight / 8)
+            image.draw(at: imagePoint)
+        }
     }
     
     class func hud(inView view: UIView, animated: Bool) -> HudView {
