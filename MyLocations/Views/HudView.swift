9c8e36c4f15843dd4d33bf290c051b261dff9318
Vanessa Flores <floresv26@gmail.com>
2020-09-02 14:06:14 -0500

    Draw the hud on the HudView

--- MyLocations/Views/HudView.swift
+++ MyLocations/Views/HudView.swift
@@ -11,15 +11,25 @@ import UIKit
 class HudView: UIView {
     var text = ""
     
+    override func draw(_ rect: CGRect) {
+        let boxWidth: CGFloat = 96
+        let boxHeight: CGFloat = 96
+        
+        let boxRect = CGRect(x: round((bounds.size.width - boxWidth) / 2), y: round((bounds.size.height - boxHeight) / 2), width: boxWidth, height: boxHeight)
+        
+        let roundedRect = UIBezierPath(roundedRect: boxRect, cornerRadius: 10)
+        
+        UIColor(white: 0.3, alpha: 0.8).setFill()
+        roundedRect.fill()
+    }
+    
     class func hud(inView view: UIView, animated: Bool) -> HudView {
         let hudView = HudView(frame: view.bounds)
         hudView.isOpaque = false
         
         view.addSubview(hudView)
         view.isUserInteractionEnabled = false
-        
-        hudView.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.5)
-        
+                
         return hudView
     }
 }
