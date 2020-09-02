47a65875efce40399c82a25e2ba67ea9dfe7556c
Vanessa Flores <floresv26@gmail.com>
2020-09-02 17:06:09 -0500

    Build data model

---
+++ MyLocations/Data Model/Location+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Vanessa Flores on 9/2/20.
+//  Copyright © 2020 Rising Dev Habits. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
---
+++ MyLocations/Data Model/Location+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  Location+CoreDataProperties.swift
+//  MyLocations
+//
+//  Created by Vanessa Flores on 9/2/20.
+//  Copyright © 2020 Rising Dev Habits. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+import CoreLocation
+
+
+extension Location {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Location> {
+        return NSFetchRequest<Location>(entityName: "Location")
+    }
+
+    @NSManaged public var latitude: Double
+    @NSManaged public var longitude: Double
+    @NSManaged public var date: Date
+    @NSManaged public var locationDescription: String
+    @NSManaged public var cateogry: String
+    @NSManaged public var placemark: CLPlacemark?
+
+}
