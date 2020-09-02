316511f53cf9a12664c9d1d03a60c11f12723de7
Vanessa Flores <floresv26@gmail.com>
2020-09-02 17:13:19 -0500

    Initialize data store on app startup

--- MyLocations/Application Delegate/SceneDelegate.swift
+++ MyLocations/Application Delegate/SceneDelegate.swift
@@ -7,10 +7,24 @@
 //
 
 import UIKit
+import CoreData
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error {
+                fatalError("Could not load data store: \(error)")
+            }
+        }
+        
+        return container
+    }()
+    
+    lazy var managedObjectContext: NSManagedObjectContext = persistentContainer.viewContext
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
