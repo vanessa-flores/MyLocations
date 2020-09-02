8f7848d2e6e318bb7d01c6d7863002517403ad8e
Vanessa Flores <floresv26@gmail.com>
2020-09-02 08:42:57 -0500

    Add Location Detail screen and LocationDetailViewController. Connect outlets

---
+++ MyLocations/View Controllers/LocationDetailsViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LocationDetailsViewController.swift
+//  MyLocations
+//
+//  Created by Vanessa Flores on 9/2/20.
+//  Copyright © 2020 Rising Dev Habits. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UITableViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet private weak var descriptionTextView: UITextView!
+    @IBOutlet private weak var categoryLabel: UILabel!
+    @IBOutlet private weak var latitudeLabel: UILabel!
+    @IBOutlet private weak var longitudeLabel: UILabel!
+    @IBOutlet private weak var addressLabel: UILabel!
+    @IBOutlet private weak var dateLabel: UILabel!
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction private func cancel() {
+        
+    }
+    
+    @IBAction private func done() {
+        
+    }
+    
+    private func selectCategory() {
+        
+    }
+    
+    private func addPhoto() {
+        
+    }
+
+}
