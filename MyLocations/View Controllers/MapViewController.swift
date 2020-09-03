7dea19e5f16c4e17d990a0289cfdd06e4fc1e241
Vanessa Flores <floresv26@gmail.com>
2020-09-03 15:51:38 -0500

    Segue to EditLocation when tapping button to showLocationDetails

--- MyLocations/View Controllers/MapViewController.swift
+++ MyLocations/View Controllers/MapViewController.swift
@@ -90,9 +90,21 @@ class MapViewController: UIViewController {
     }
     
     @objc private func showLocationDetails(_ sender: UIButton) {
-        
+        performSegue(withIdentifier: "EditLocation", sender: sender)
     }
     
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EditLocation" {
+            let controller = segue.destination as! LocationDetailsViewController
+            controller.managedObjectContext = managedObjectContext
+            
+            let button = sender as! UIButton
+            let location = locations[button.tag]
+            controller.locationToEdit = location
+        }
+    }
 }
 
 // MARK: - MKMapViewDelegate
