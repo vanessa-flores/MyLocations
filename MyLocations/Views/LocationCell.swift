01f5894faca57cd994bfd69da0e34b73a4f8fd04
Vanessa Flores <floresv26@gmail.com>
2020-09-03 12:35:02 -0500

    Use custom table view cell for LocationCell

---
+++ MyLocations/Views/LocationCell.swift
@@ -0,0 +1,57 @@
+//
+//  LocationCell.swift
+//  MyLocations
+//
+//  Created by Vanessa Flores on 9/3/20.
+//  Copyright © 2020 Rising Dev Habits. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    @IBOutlet private weak var addressLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    // MARK: - Helpers
+    
+    func configure(for location: Location) {
+        if location.locationDescription.isEmpty {
+            descriptionLabel.text = "(No Description)"
+        } else {
+            descriptionLabel.text = location.locationDescription
+        }
+        
+        if let placemark = location.placemark {
+            var text = ""
+            
+            if let s = placemark.subThoroughfare {
+                text += s + " "
+            }
+            if let s = placemark.thoroughfare {
+                text += s + ", "
+            }
+            if let s = placemark.locality {
+                text += s
+            }
+            
+            addressLabel.text = text
+        } else {
+            addressLabel.text = String(format: "Lat: %.8f, Long: %.8f", location.latitude, location.longitude)
+        }
+    }
+
+}
