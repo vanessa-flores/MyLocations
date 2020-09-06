e83831c8d8a2ba2eed8b289863a84af439d7d038
Vanessa Flores <floresv26@gmail.com>
2020-09-05 19:03:57 -0500

    Resize to aspect fill instead of aspect fit

--- MyLocations/Utils/UIImage+Resize.swift
+++ MyLocations/Utils/UIImage+Resize.swift
@@ -12,7 +12,7 @@ extension UIImage {
     func resized(withBounds bounds: CGSize) -> UIImage {
         let horizontalRatio = bounds.width / size.width
         let verticalRatio = bounds.height / size.height
-        let ratio = min(horizontalRatio, verticalRatio)
+        let ratio = max(horizontalRatio, verticalRatio)
         let newSize = CGSize(width: size.width * ratio, height: size.height * ratio)
         
         UIGraphicsBeginImageContextWithOptions(newSize, true, 0)
