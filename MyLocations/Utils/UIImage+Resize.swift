5cc38ff336b45bda05ca968d28220d64fb3088ac
Vanessa Flores <floresv26@gmail.com>
2020-09-05 19:00:38 -0500

    Resize images

---
+++ MyLocations/Utils/UIImage+Resize.swift
@@ -0,0 +1,25 @@
+//
+//  UIImage+Resize.swift
+//  MyLocations
+//
+//  Created by Vanessa Flores on 9/5/20.
+//  Copyright © 2020 Rising Dev Habits. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    func resized(withBounds bounds: CGSize) -> UIImage {
+        let horizontalRatio = bounds.width / size.width
+        let verticalRatio = bounds.height / size.height
+        let ratio = min(horizontalRatio, verticalRatio)
+        let newSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, true, 0)
+        draw(in: CGRect(origin: CGPoint.zero, size: newSize))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+}
